d5f2b381f63c8d194e3147386a3ce67526dd7f8a
Eric Seitz <eric.seitz@gmail.com>
2021-07-01 11:29:00 -0700

    Changes to ViewController so CLLocationManager can be initialized

--- esoEs/Controllers/ViewController.swift
+++ esoEs/Controllers/ViewController.swift
@@ -7,11 +7,20 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
-
+    
+    private lazy var locationManager: CLLocationManager = {
+        
+        let manager = CLLocationManager()
+        manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        manager.distanceFilter = kCLDistanceFilterNone
+        return manager
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
